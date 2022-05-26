a2dd4aab628de167770032e8738add18c85e5bde
Marek <531devv@gmail.com>
2022-05-26 19:30:15 +0200

    Adding useContentful component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import useContentful from "./components/useContentful";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({});
+  const { getPosts } = useContentful();
+  useEffect(() => {
+    getPosts().then((response) => setData(response));
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/useContentful.js
@@ -0,0 +1,34 @@
+import { createClient } from "contentful";
+
+const useContentful = () => {
+  const client = createClient({
+    space: "y0pcq4bx31f2",
+    accessToken: "4RYsvPDo36si31T0D-4l9mwETEaPF0_y255znm9vumc",
+    host: "preview.contentful.com",
+  });
+
+  const getPosts = async () => {
+    try {
+      const entries = await client.getEntries({
+        content_type: "blogPost",
+        select: "fields",
+      });
+
+      const sanitanizedEntries = entries.items.map((item) => {
+        const image = item.fields.mainImage.fields.file.url;
+        return {
+          ...item.fields,
+          image,
+        };
+      });
+
+      return sanitanizedEntries;
+    } catch (error) {
+      console.log(`Error fetching authors ${error}`);
+    }
+  };
+
+  return { getPosts };
+};
+
+export default useContentful;
